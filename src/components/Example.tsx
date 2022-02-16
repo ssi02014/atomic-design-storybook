d2b16c9aa7fe119fd87455e5d3e3275d560c3ccf
JeonMinJae <ssi02014@naver.com>
2022-02-16 12:48:11 +0900

    feat: storybook

---
+++ src/components/Example.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Example = () => {
+  return (
+    <div>
+      <button>예제</button>
+    </div>
+  );
+};
+
+export default Example;
---
+++ src/stories/Example.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story } from "@storybook/react/types-6-0";
+import Example from "../components/Example";
+
+export default {
+  title: "Components/Button",
+  component: Example,
+};
+
+const Template: Story = ({}) => {
+  return (
+    <div>
+      <Example />
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
